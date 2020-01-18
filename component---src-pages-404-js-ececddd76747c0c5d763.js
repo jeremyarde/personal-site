405399a44f3659f22a797efb9f4e77f6a003b48f
Jeremy <jere.arde@gmail.com>
2020-01-18 16:50:37 -0600

    setup develop branch

--- component---src-pages-404-js-ececddd76747c0c5d763.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{w2l6:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),r=t.n(o);e.default=function(){return r.a.createElement("div",null,"Sorry, page not found ( 404 )")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-ececddd76747c0c5d763.js.map
