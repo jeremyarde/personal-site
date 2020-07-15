00e8e0bb6748d8c5a8f3d1751302ee626fdcb0af
Jeremy <jere.arde@gmail.com>
2020-07-14 20:47:25 -0500

    Updates

--- component---src-pages-about-js-cb9fa06e092b5360d8ff.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"3XHS":function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return r}));var a=t("q1tI"),u=t.n(a),l=t("BoQL");function r(e){return u.a.createElement("div",null,u.a.createElement(l.a,null),u.a.createElement("div",null,"About page"))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-cb9fa06e092b5360d8ff.js.map
