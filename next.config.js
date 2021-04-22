71fd394898e2476d2bb54eaf1292170d7e32476e
Jeremy <jere.arde@gmail.com>
2021-04-22 08:21:08 -0500

    remove some files from public directory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const assetPrefix = process.env.BUILDING_FOR_NOW ? "/personal-site" : "";
+// const assetPrefix = process.env.BUILDING_FOR_NOW ? "jeremyarde.github.io" : "";
 
 module.exports = {
   assetPrefix,
